475cbada19f3f4c8167db3f65933d38a361532aa
Arthur <arthurvicencio@bitbucket.com>
2021-06-16 04:36:33 +0800

    add time spent in report

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,7 +12,12 @@ import (
 )
 
 type Log struct {
-	Name      string
+	Name     string
+	Duration int
+}
+
+type LogSummary struct {
+	Name string
 	TimeSpent string
 }
 
@@ -27,7 +33,8 @@ func logWindow() {
 		CREATE TABLE IF NOT EXISTS log (
 		   id INTEGER PRIMARY KEY AUTOINCREMENT,
 		   name VARCHAR(500),
-		   created DATETIME DEFAULT CURRENT_TIMESTAMP
+		   started DATETIME DEFAULT CURRENT_TIMESTAMP,
+		   ended DATETIME
 		);
 	`)
 
@@ -35,6 +42,14 @@ func logWindow() {
 	for {
 		if window := GetWindow(); currentWindow != window {
 
+			db.Exec(`
+				UPDATE log
+				SET ended = CURRENT_TIMESTAMP
+				WHERE id = (
+					SELECT MAX(id)
+					FROM log
+				);
+			`)
 			db.Exec(
 				fmt.Sprintf(`INSERT INTO log(name) VALUES ("%s")`, window),
 			)
@@ -47,7 +62,17 @@ func logWindow() {
 func getLogs() []Log {
 	logs := make([]Log, 0)
 
-	stmt, err := db.Prepare("SELECT name FROM log;")
+	stmt, err := db.Prepare(`
+		SELECT
+			name,
+			SUM(
+				strftime("%s", IFNULL(ended, CURRENT_TIMESTAMP)) -
+				strftime("%s",started)
+			)
+		FROM log
+		GROUP BY name
+		ORDER BY name;
+	`)
 	if err != nil {
 		return logs
 	}
@@ -62,6 +87,7 @@ func getLogs() []Log {
 
 		err = rows.Scan(
 			&log.Name,
+			&log.Duration,
 		)
 		if err != nil {
 			return nil
@@ -73,12 +99,45 @@ func getLogs() []Log {
 	return logs
 }
 
+func secondsToTimeFormat(seconds int) string {
+	text := []string{"00","00","00"}
+
+	s := seconds / (60*24)
+	if s > 0 {
+		text[0] = fmt.Sprintf("%02d", s)
+	}
+	seconds = seconds % (60*24)
+
+	s = seconds / 60
+	if s > 0 {
+		text[1] = fmt.Sprintf("%02d", s)
+	}
+	seconds = seconds % 60
+
+	if seconds > 0 {
+		text[2] = fmt.Sprintf("%02d", seconds)
+	}
+
+	return strings.Join(text, ":")
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("index.html")
 
-		t.Execute(w, getLogs())
+
+		logSummary := make([]LogSummary, 0)
+		logs := getLogs()
+
+		for _, lg := range logs {
+			logSummary = append(logSummary, LogSummary{
+				Name: lg.Name,
+				TimeSpent: secondsToTimeFormat(lg.Duration),
+			})
+		}
+
+		t.Execute(w, logSummary)
 	})
 
 	go logWindow()
